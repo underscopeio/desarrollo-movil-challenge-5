353fcdb1d13e6e51f82d99eabc241aba4a48a556
Carlos Castro <charly@MacBook-Pro-de-Carlos.local>
2018-09-15 15:52:55 -0300

    Versión Final

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import EjemploFlex from './src/EjemploFlex'
+
+// import EjemploFlex from './src/Ejercicio1'
+// import EjemploFlex from './src/Ejercicio2'
+// import EjemploFlex from './src/Ejercicio3'
+//import EjemploFlex from './src/Ejercicio4'
+//import EjemploFlex from './src/Ejercicio5'
+//import EjemploFlex from './src/Ejercicio6'
+//import EjemploFlex from './src/Ejercicio7'
+import EjemploFlex from './src/Ejercicio8'
 
 export default class App extends React.Component {
   render() {
--- src/Cuadrado.js
+++ src/Cuadrado.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
   },
 
   grande: {
-    width: 100,
-    height: 100,
+    width: 120,
+    height: 120,
   },
 })
