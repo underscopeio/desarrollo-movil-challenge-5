ba3037930a3cc93d316dc6000dd7c7747cd53955
Juannito Gonzalez <juannito@gmail.com>
2018-09-15 15:20:42 -0300

    2) Haciendo la vertical

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import EjemploFlex from './src/EjemploFlex01'
+import EjemploFlex from './src/EjemploFlex02'
 
 export default class App extends React.Component {
   render() {
