74cf3ef99f9114959d18578c0ab79d2f260473ca
Braian Ledesma <braian.ledesma-99@hotmail.com>
2018-09-15 15:21:52 -0300

    3. Centrado perfecto

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View } from 'react-native'
 import EjemploFlex from './src/EjemploFlex'
 import FilaIndia from './src/Ejercicio1'
 import HaciendoLaVertical from './src/Ejercicio2'
+import CentradoPerfecto from './src/Ejercicio3'
 
 export default class App extends React.Component {
   render() {
-    return <HaciendoLaVertical />
+    return <CentradoPerfecto />
   }
 }
 
