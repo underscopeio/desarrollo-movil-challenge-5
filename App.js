0d08b79f847b32121fea3bb17d5fa26a82c08dc8
Braian Ledesma <braian.ledesma-99@hotmail.com>
2018-09-15 15:15:51 -0300

    2. Haciendo la inversa

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import EjemploFlex from './src/EjemploFlex'
 import FilaIndia from './src/Ejercicio1'
+import HaciendoLaVertical from './src/Ejercicio2'
 
 export default class App extends React.Component {
   render() {
-    return <FilaIndia />
+    return <HaciendoLaVertical />
   }
 }
 
