eabfe43bf3fa0cb2ba7c95582f2940ba05fb5bd3
Braian Ledesma <braian.ledesma-99@hotmail.com>
2018-09-15 15:11:51 -0300

    Fila india

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import EjemploFlex from './src/EjemploFlex'
+import FilaIndia from './src/Ejercicio1'
 
 export default class App extends React.Component {
   render() {
-    return <EjemploFlex />
+    return <FilaIndia />
   }
 }
 
