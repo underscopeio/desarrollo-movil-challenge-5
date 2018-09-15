7b68a175143d9bf5c9b075f506d74f9532043314
CarlosPorta77 <carlos.porta@innovationlab.cc>
2018-09-15 15:29:50 -0300

    Los primeros 5

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import EjemploFlex from './src/EjemploFlex'
+import EjemploFlex from './src/ejercicio5'
 
 export default class App extends React.Component {
   render() {
