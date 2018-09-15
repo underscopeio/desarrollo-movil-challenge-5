837160943a0dc5b4da488af51b91da2a736b7c38
Juannito Gonzalez <juannito@gmail.com>
2018-09-15 15:15:22 -0300

    Fila india

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import EjemploFlex from './src/EjemploFlex'
+import EjemploFlex from './src/EjemploFlex01'
 
 export default class App extends React.Component {
   render() {
