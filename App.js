fea5b395553671475f92d8c5b551ac793a7d9f49
Roldan Ucedo <nicolasroldan1994@gmail.com>
2018-09-22 00:12:02 -0300

    Ejercicio3

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import Ejercicio2 from './src/Ejercicio2'
+import Ejercicio3 from './src/Ejercicio3'
 
 export default class App extends React.Component {
   render() {
-    return <Ejercicio2 />
+    return <Ejercicio3 />
   }
 }
 
